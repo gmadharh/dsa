2f76e7e16a66e40a2a34565fe8e1fc8a94bf4cf1
Gevin Madharha <58638508+gmadharh@users.noreply.github.com>
2024-06-18 11:58:39 -0400

    Time: 8 ms (78.1%), Space: 15.8 MB (7.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
+        
         std::unordered_map<int,int> mp;
+        int n = nums.size();
 
         std::vector<int> ans;
-
-        for(int i = 0;i < nums.size();i++){
-
-            // target - nums[i] == in map
-            // 9 - 2 == 7 in map
-            if(mp.find(target - nums[i]) != mp.end()){
+        for(int i = 0;i < n;i++){
+            if(mp.count(target - nums[i]) > 0){
                 ans.push_back(i);
-                ans.push_back(mp[target - nums[i]]);
-                break;
+                ans.push_back(mp[target-nums[i]]);
             }
 
             mp[nums[i]] = i;
         }
         return ans;
+
     }
 };
