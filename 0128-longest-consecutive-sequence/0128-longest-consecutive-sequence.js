6b38bd93b0cb18d4e76f9ad2a7719616ef8d0244
Gevin Madharha <58638508+gmadharh@users.noreply.github.com>
2024-08-26 11:23:27 -0400

    Time: 1425 ms (5.04%), Space: 73.3 MB (37.32%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    
+    // use Set for lookup
+
+    const numsSet = new Set(nums)
+
+    let count = 1
+    let sequence = 0
+    for(let i = 0;i < nums.length;i++){
+
+        if(!numsSet.has(nums[i]-1)){
+
+            while(numsSet.has(nums[i] + count)){
+                count++;
+            }
+
+
+            sequence = Math.max(count, sequence)
+            count = 0
+        }
+    }
+
+    return sequence
+
+
+};
